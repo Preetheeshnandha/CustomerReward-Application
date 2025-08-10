c230921fbc40875f2cb54246ab21a6585dfbc5eb
Preetheeshnandha G <preetheeshnandha@gmail.com\>
2025-08-10 23:13:43 +0530

    Added sample data to the data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,32 @@
-
-
-
 insert into customers(customer_name,customer_email)
 values ('Raj','raj@gmail.com');
-
 
 insert into customers(customer_name,customer_email)
 values ('Ram','ram@gmail.com');
 
-
 insert into customers(customer_name,customer_email)
 values ('Sam','sam@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Ragul','ragul@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Kumar','kumar@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Guna','guna@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Baskar','baskar@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Rakesh','rakesh@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Samir','samir@gmail.com');
+
+insert into customers(customer_name,customer_email)
+values ('Mani','mani@gmail.com');
 
 insert into transactions(transaction_date,transaction_amount,customer_id)
 values ('2025-07-12',100,1);
@@ -24,6 +40,32 @@ values ('2025-05-18',200,1);
 insert into transactions(transaction_date,transaction_amount,customer_id)
 values ('2025-04-21',50,3);
 
-
 insert into transactions(transaction_date,transaction_amount,customer_id)
 values ('2025-06-25',1000,2);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-04-01',250,5);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-07-12',500,5);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-03-11',500,6);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-04-19',5500,7);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-03-12',500,9);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-01-10',500,9);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-06-05',600,10);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-07-12',500,5);
+
+insert into transactions(transaction_date,transaction_amount,customer_id)
+values ('2025-07-12',500,5);
